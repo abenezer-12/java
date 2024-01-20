d9ef2fb6c9efcbee5af6d6b946faea36e574c0ef
abenezer-12 <tabenezer2000@gmail.com>
2024-01-20 23:43:35 +0300

    php arithemetic

---
+++ Arithmetic operation.php
@@ -0,0 +1,60 @@
+<?php
+$x=23;
+$y=12;
+echo "the sum of 23 and 12 is : " ;
+$z=$x+$y;
+echo $z;
+echo "</br>";
+
+echo "the difference  of 23 and 12 is : " ;
+$z=$x-$y;
+echo $z;
+echo "</br>";
+echo "the produt  of 23 and 12 is : " ;
+$z=$x*$y;
+echo $z;
+echo "</br>";
+echo "the queient  of 23 and 12 is : " ;
+$z=$x/$y;
+echo $z;
+echo "</br>";
+echo "the modulus of 23 and 12 is : " ;
+$z=$x%$y;
+echo $z;
+echo "</br>";
+echo "the exponential of 23 and 12 is : " ;
+$z=$x**$y;
+echo $z;
+echo "</br>";
+echo "##################################################################";
+echo "</br>";
+echo "assigment operator";
+echo "</br>";
+$x=6;
+$y=4;
+echo "the value of x is 6 but y asinged on it so x is 4  :";
+echo $x=$y;
+echo "</br>";
+echo "x=6 and y=4 the vaue of x*=y is : ";
+echo $x*=$y;
+echo "</br>"; 
+echo "****************************************";
+$a=4;
+$b=5;
+echo "</br>";
+echo "if a=4 and b=5 the what is value of a&&b is  : ";
+echo var_dump($a==6 && $b==4);
+echo "</bra>";
+echo "*******************sting operator************";
+$s='abenezer' ;
+$t='yohannes' ;
+echo "</br>";
+echo $s.$t;
+echo "</br>";
+echo sqrt(34);
+echo "</br>";
+echo "</br>";
+echo rand();
+
+
+?>
